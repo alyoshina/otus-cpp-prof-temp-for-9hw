3a8423c48fa9e263cdb48d895ac9b11987f57833
Liliya <alyoshina.li@gmail.com>
2024-01-17 16:01:40 +0300

    fix5

--- _AsyncConfig.cmake
+++
@@ -1,3 +0,0 @@
-add_library(async SHARED IMPORTED)
-find_library(ASYNC_LIBRARY_PATH async HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
-set_target_properties(async PROPERTIES IMPORTED_LOCATION "${ASYNC_LIBRARY_PATH}")
